fbf42df22c804888c6846133b56d0345bbee154b
Nadine <nadine.spruegel@web.de>
2020-11-18 10:36:22 +0100

    Ne Variable umbenannt

--- Uebung1/Code/odefun.m
+++ Uebung1/Code/odefun.m
@@ -1,4 +1,4 @@
-function dydt2 =odefun(t,y,dc)
+function dydt =odefun(t,y,dc)
 % Funktion für die nummerische Integration von GOCE
 % Nadine Sprügel 3317570
 % Ziqing Yu 3218051
@@ -6,5 +6,5 @@ function dydt2 =odefun(t,y,dc)
 GM=3.9865005e14;
 h_GOCE = 225 * 1000; % km
 f1_GOCE = drag_force(dc,h_GOCE,[y(4);y(5);y(6)]);
-dydt2=[y(4); y(5); y(6); -y(1)*GM/norm(y(1:3))^3 + f1_GOCE(1); -y(2)*GM/norm(y(1:3))^3 + f1_GOCE(2); -y(3)*GM/norm(y(1:3))^3 + f1_GOCE(3)];
+dydt=[y(4); y(5); y(6); -y(1)*GM/norm(y(1:3))^3 + f1_GOCE(1); -y(2)*GM/norm(y(1:3))^3 + f1_GOCE(2); -y(3)*GM/norm(y(1:3))^3 + f1_GOCE(3)];
 end
